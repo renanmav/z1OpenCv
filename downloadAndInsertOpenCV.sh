8ae84ee3c321cc4fac7853b840cd5406dd77f739
Renan Machado <renan.mav@hotmail.com>
2021-03-26 15:27:48 -0300

    avoid creating a inner libs folder when extracting jniLibs

--- downloadAndInsertOpenCV.sh
+++ downloadAndInsertOpenCV.sh
@@ -1,9 +1,7 @@
-# taken from https://github.com/brainhubeu/react-native-opencv-tutorial/blob/master/downloadAndInsertOpenCV.sh
-
-# ios
 version=3.4.11
 base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${version}/
 
+# ios
 curl -LO ${base_url}/opencv-${version}-ios-framework.zip
 unzip -a opencv-${version}-ios-framework.zip
 cd ios
@@ -13,12 +11,11 @@ rm -rf opencv-${version}-ios-framework.zip
 rm -rf opencv2.framework/
 
 # android
-
 curl -LO ${base_url}/opencv-${version}-android-sdk.zip
 unzip opencv-${version}-android-sdk.zip
 cd android/openCVLibrary3411/src/main
 mkdir jniLibs
-cp -r ./../../../../OpenCV-android-sdk/sdk/native/libs/ ./jniLibs
+cp -r ./../../../../OpenCV-android-sdk/sdk/native/libs/* ./jniLibs
 cd ../../../../
 rm -rf opencv-${version}-android-sdk.zip
 rm -rf OpenCV-android-sdk/

6f10952f90bff35a716034020e20176878d0eb43
Renan Machado <renan.mav@hotmail.com>
2021-03-26 09:32:52 -0300

    correctly use curl to fetch opencv

--- downloadAndInsertOpenCV.sh
+++ downloadAndInsertOpenCV.sh
@@ -4,9 +4,7 @@
 version=3.4.11
 base_url=https://razaoinfo.dl.sourceforge.net/project/opencvlibrary/${version}/
 
-apt-get install wget
-
-wget ${base_url}/opencv-${version}-ios-framework.zip
+curl -LO ${base_url}/opencv-${version}-ios-framework.zip
 unzip -a opencv-${version}-ios-framework.zip
 cd ios
 cp -r ./../opencv2.framework ./
@@ -16,7 +14,7 @@ rm -rf opencv2.framework/
 
 # android
 
-wget ${base_url}/opencv-${version}-android-sdk.zip
+curl -LO ${base_url}/opencv-${version}-android-sdk.zip
 unzip opencv-${version}-android-sdk.zip
 cd android/openCVLibrary3411/src/main
 mkdir jniLibs
